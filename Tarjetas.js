4dc5b900f0a2bf820bf6b7d0c9f2d01742dfc3aa
ManuCanello <canellomanuel2@gmail.com>
2025-10-03 14:16:50 -0300

    Update Tarjetas.js

--- Tarjetas.js
+++ Tarjetas.js
@@ -6,78 +6,53 @@ class Tarjetas {
     }
 
     createCard(img, title, description) {
-        const html = `
-        <div class="cardContainer" id="card${this.id}" onclick="new Tarjetas().marcarCard(card${this.id})">
-            <img src=${img}></img>
+
+        const card = document.createElement("div");
+        card.className = "cardContainer";
+        card.id = `card${this.id}`;
+
+        card.innerHTML = `
+            <img src="${img}">
             <div class="cardText"> 
                 <div class="cardTitle">${title}</div>
                 <div class="cardDescription">${description}</div>
             </div>
-        </div>`;
+        `;
+
+        card.addEventListener("click", () => this.marcarCard(card));
+
         this.id++;
-        return html;
+        return card;
     }
 
+    
     showCards(selectedDate) {
-        let listadoComida = "";
+        this.container.innerHTML = ""; 
 
-
-        //Aca haria una consulta sql
+        // "Consulta"
         const cardJson = [
-            {
-                fecha: "2025-10-06",
-                datos: {
-                    img: "https://picsum.photos/200/120?random=1",
-                    title: "Pizza",
-                    description: "Pizza con queso, tomate y albahaca"
-                }
-            },
-            {
-                fecha: "2025-10-06",
-                datos: {
-                    img: "https://picsum.photos/200/120?random=2",
-                    title: "Hamburguesa",
-                    description: "Hamburguesa simple con papas fritas"
-                }
-            },
-            {
-                fecha: "2025-10-06",
-                datos: {
-                    img: "https://picsum.photos/200/120?random=3",
-                    title: "Ensalada",
-                    description: "Ensalada fresca con lechuga, tomate y zanahoria"
-                }
-            },
-               {
-                fecha: "2025-10-07",
-                datos: {
-                    img: "https://picsum.photos/200/120?random=1",
-                    title: "Asado",
-                    description: "Asaduti"
-                }
-            },
-            {
-                fecha: "2025-10-07",
-                datos: {
-                    img: "https://picsum.photos/200/120?random=3",
-                    title: "Pes cado",
-                    description: "Pesezuli"
-                }
-            }
+            { fecha: "2025-10-06", datos: { img: "https://picsum.photos/200/120?random=1", title: "Pizza", description: "Pizza con queso, tomate y albahaca" } },
+            { fecha: "2025-10-06", datos: { img: "https://picsum.photos/200/120?random=2", title: "Hamburguesa", description: "Hamburguesa simple con papas fritas" } },
+            { fecha: "2025-10-06", datos: { img: "https://picsum.photos/200/120?random=3", title: "Ensalada", description: "Ensalada fresca con lechuga, tomate y zanahoria" } },
+            { fecha: "2025-10-07", datos: { img: "https://picsum.photos/200/120?random=1", title: "Asado", description: "Asaduti" } },
+            { fecha: "2025-10-07", datos: { img: "https://picsum.photos/200/120?random=3", title: "Pescado", description: "Pesezuli" } }
         ];
 
         const resultados = cardJson.filter(item => item.fecha === selectedDate);
 
+        if(resultados.length == 0){
+                this.container.innerHTML = "<h1>Aun no hay menus cargados</h1>"
+                return;
+        }
+
         for (let i = 0; i < resultados.length; i++) {
-            
-            listadoComida += this.createCard(
+            const card = this.createCard(
                 resultados[i].datos.img,
                 resultados[i].datos.title,
                 resultados[i].datos.description
             );
+            this.container.appendChild(card);
         }
-
-        this.container.innerHTML = listadoComida;
 
         new Sortable(this.container, {
             swapThreshold: 1,
@@ -96,10 +71,15 @@ class Tarjetas {
                 }
             });
             id.classList.add("checkedCard");
+            this.container.prepend(id)
         } else {
             id.classList.remove("checkedCard");
             this.lastId = "";
         }
+    }
+
+    showCardsByJson(){
+        //Implementar
     }
 }
 
